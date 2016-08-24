efebcafb237aeb7aff7e0f3d9d3ca5e8097197b4
kurtzberger <kurtzberger@gmail.com>
2016-08-24 00:59:05 -0600

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -3,44 +3,50 @@
  * Author: 		Eric Kurtzberg
  * Description:	initialization javascript for all pages
  */
- 
+
+const LOAD_DELAY = 750; // in milliseconds
+var curUser = null;
+var UID = null;
+var season = 2016; // update this each season. This is used for the individual week league picks.
+				   // it's set static here instead of reading from NFL.com to save overhead.
+
 // Initialize Firebase
 var config = {
-	apiKey: "AIzaSyDfTFjhKTTiK8NZbeVDCAs6eRVIFsAENfI",
-	authDomain: "crackling-heat-2839.firebaseapp.com",
-	databaseURL: "https://crackling-heat-2839.firebaseio.com",
-	storageBucket: "crackling-heat-2839.appspot.com",
+apiKey: "AIzaSyAOuHBEOKT9SIpsAChi7Z-gs6iJD5jp3jM",
+authDomain: "nfl-weekly-pick-app.firebaseapp.com",
+databaseURL: "https://nfl-weekly-pick-app.firebaseio.com",
+storageBucket: "nfl-weekly-pick-app.appspot.com",
 };
 firebase.initializeApp(config);
-
-
 var sPath = window.location.pathname;
-var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
-if(sPage != "index.html")
+var sPage = sPath.substring(sPath.lastIndexOf('pick/') + 5).slice(0, -1);	// get just the page name with no slashes
+
+if(sPage != "register")
 {
-	$(".button-collapse").sideNav();
-	$(".collapsible").collapsible({
-		accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+	firebase.auth().onAuthStateChanged(function(user)	// on log in or log out
+	{
+		if(user)
+		{
+			curUser = user;
+			UID = createUID(user.email);
+			if(sPage == "index.html" || sPage == "")	//user is logged in
+				window.location.href = "standings";
+			else if(sPage == "change-password")
+				$("#current-user").html("Current user: <b>" + user.email + "</b>");
+			else if(sPage == "user-picks")
+			{
+				var week = location.search.substring(1).split("&")[0].split("=")[1];
+				$("#title").append(curUser.displayName + "'s " + season + " Week " + week + " Picks");
+			}
+		}
+		else if(user == null)
+		{
+			if(sPage == "login/forgot-password.htm");
+			else if(sPage == "register");
+			else if( sPage != "login")
+				// No user is signed in.
+				if(sPage == "index.html" || sPage == "") setTimeout(function() { window.location.href = "login"; }, 600);
+				else	setTimeout(function() { window.location.href = "../login"; }, 600);
+		}
 	});
-
-	for(var i=1; i<=17; i++)
-	{
-		$("#weeks-list").append("<li><a class='waves-effect'>Week "+i+"</a></li>");
-	}
 }
-firebase.auth().onAuthStateChanged(function(user)
-{
-	if(user != null && sPage == "index.html")
-	{
-		//user is logged in
-		window.location.href = "standings.html";
-	}
-	if(user == null)
-	{
-		if(sPage == "forgot-password.html");
-		else if( sPage != "login.html")
-			// No user is signed in.
-			window.location.href = "login.html";
-	}
-}); 
-
---
+++ js/user-picks.js
@@ -0,0 +1,89 @@
+$(document).ready(function()
+{
+	$("#header").load("../header.html", function()
+	{
+		var week, Games, localURL, seasonType, /*season,*/ path;
+		week = location.search.substring(1).split("&")[0].split("=")[1];
+		// this URL is used just for this webpage
+		localURL = 'http://www.nfl.com/ajax/scorestrip?season=' + season + '&seasonType=REG&week=' + week;
+		
+		$('.your-picks').addClass("deep-orange lighten-3");
+		$("#user-picks-week" + week + "-link").addClass("deep-orange lighten-3");
+		
+		$("#user-picks-link").addClass("deep-orange lighten-3");
+		// add loader animation
+		$("#main-content").append('<div class="loader s12 m4 center">' +
+				'<div class="preloader-wrapper big active">' +
+				'<div class="spinner-layer spinner-blue-only">' +
+				'<div class="circle-clipper left">' +
+				'<div class="circle"></div>' +
+				'</div><div class="gap-patch">' +
+				'<div class="circle"></div>' + 
+				'</div><div class="circle-clipper right">' +
+				'<div class="circle"></div></div></div></div></div>');
+
+		// Get a reference to the database service
+		var database = firebase.database();
+		
+		// read from nfl.com all the games for the given URL defined in init.js
+		$.get(localURL, function( data )
+		{
+			seasonType = $(data).find('gms')[0].getAttribute('t');
+			path = season + '/picks/week' + week;
+			// load xml data into the table
+			setTimeout(function() {
+				xmlImport(data);
+				databaseImport(database, path, UID);
+				disableStartedGames();
+				//remove loader animation
+				$(".loader").remove();
+				$(".show-me").toggle();
+			}, LOAD_DELAY);
+		});
+		
+		$('#main-content').on('change', 'select', function()
+		{
+			// correctly handle all point assignment events, i.e., don't allow duplicate point assignments
+			setTimeout(function() { pointAssignments(); }, 150);
+		});
+		
+		$('#main-content').on("click", "#submit-picks", function()
+		{
+			var success = false;
+			$("#submit-div").html('<div class="loader s12 m4 center">' +
+				'<div class="preloader-wrapper big active">' +
+				'<div class="spinner-layer spinner-blue-only">' +
+				'<div class="circle-clipper left">' +
+				'<div class="circle"></div>' +
+				'</div><div class="gap-patch">' +
+				'<div class="circle"></div>' + 
+				'</div><div class="circle-clipper right">' +
+				'<div class="circle"></div></div></div></div></div>');
+			setTimeout(function()
+			{ 
+				success = submitPicks(database, path, UID);
+				if(success)
+				{
+					$("#main-content").html("");
+					$("#modal-text").html("<h4>Picks submitted!</h4><p>Your picks have been submitted</p>");
+					$("#modal-confirm").text(funnyPhrase());
+					$("#notification").openModal({
+						complete: function() { $("#week" + week + "-link").trigger("click"); } }); // Callback for Modal close
+				} else $('#submit-div').html('<a id="submit-picks" class="btn waves-effect waves-light blue-grey lighten-1 col s12"><i class="mdi mdi-send right"></i>Submit</a>');
+			}, LOAD_DELAY);
+		});
+		
+		$('#main-content').on('click', '.reset-game', function()
+		{
+			var temp = $(this).attr('id').substring($(this).attr('id').indexOf('-')+1);	//temp variable for game identifier
+			$('input:radio[name=game' + temp +']').prop('checked', false);	// clear team pick
+			$('#dropdown-' + temp).val('');	// clear point value
+			pointAssignments(); // update point values
+			
+		});
+		
+		// call this function regularly to disable any games that have started
+		setInterval(function(){ disableStartedGames() }, 1000);
+	});
+	
+});
